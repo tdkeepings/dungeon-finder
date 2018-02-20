1d6c82eb4c1c743c87c35453b6ea967eaa34377a
Tom <tdkeepings@gmail.com>
2018-02-20 15:36:37 +0000

    removed orchestrator and added AutoFac to the solution

--- GroupFinder/Program.cs
+++ GroupFinder/Program.cs
@@ -2,40 +2,27 @@
 using System;
 using Services;
 using Lookups;
+using Autofac;
 
 namespace GroupFinder
 {
     class Program
     {
+        private static IContainer Container { get;set; }
+
         static void Main(string[] args)
         {
-            CharacterGenerator generator = new CharacterGenerator();
-            IQueueService service = new QueueService();
-
-            Console.WriteLine("Add players to the queue. W for Warrior, P for Priest");
-
-            string input = "";
-
-            while ((input = Console.ReadLine()) != "x")
+            var builder = new ContainerBuilder();
+            builder.RegisterType<QueueService>().As<IQueueService>();
+            builder.RegisterType<DungeonFinderService>();
+            Container = builder.Build();
+            
+            using (var scope = Container.BeginLifetimeScope())
             {
-                if (input.ToLower() == "w")
-                {
-                    Character c = generator.Warrior();
-                    service.AddCharacter(c);
-
-                    Console.WriteLine("Added new Warrior: " + c.Name);
-                }
-                if (input.ToLower() == "p")
-                {
-
-                    Character c = generator.Priest();
-                    service.AddCharacter(c);
-
-                    Console.WriteLine("Added new Priest: " + c.Name);
-
-                    service.AddCharacter(generator.Priest());
-                }
+                var service  = scope.Resolve<DungeonFinderService>();
+                service.Start();
             }
+            
         }
     }
 }
---
+++ Services/DungeonFinderService.cs
@@ -0,0 +1,49 @@
+﻿using Lookups;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class DungeonFinderService
+    {
+        private IQueueService _queueService;
+
+        public DungeonFinderService(IQueueService queueService)
+        {
+            this._queueService = queueService;
+        }
+
+        public void Start()
+        {
+            CharacterGenerator generator = new CharacterGenerator();
+            IQueueService service = new QueueService();
+
+            Console.WriteLine("Add players to the queue. W for Warrior, P for Priest");
+
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "x")
+            {
+                if (input.ToLower() == "w")
+                {
+                    Character c = generator.Warrior();
+                    service.AddCharacter(c);
+
+                    Console.WriteLine("Added new Warrior: " + c.Name);
+                }
+                if (input.ToLower() == "p")
+                {
+
+                    Character c = generator.Priest();
+                    service.AddCharacter(c);
+
+                    Console.WriteLine("Added new Priest: " + c.Name);
+
+                    service.AddCharacter(generator.Priest());
+                }
+            }
+        }
+    }
+}
